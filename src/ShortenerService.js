a93a059ce31c262564ce14420503bfc669b2143d
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-07-09 20:54:52 +0800

    Initial commit

---
+++ src/ShortenerService.js
@@ -0,0 +1,20 @@
+class ShortenerServive {
+  constructor(redisClient, codeSupplier) {
+    this.redisClient = redisClient;
+    this.codeSupplier = codeSupplier;
+  }
+
+  shorten = (url, cb) => {
+    const code = this.codeSupplier();
+    this.redisClient.set(code, url, (err, result) => {
+      cb(err, result, code);
+    });
+  }
+
+  get = (code, cb) => {
+    this.redisClient.get(code, (err, value) => {
+      cb(err, value);
+    });
+  }
+}
+module.exports = ShortenerServive;
