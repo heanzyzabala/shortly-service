8d797a7f70579921470705ab5981c9c739f2143a
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-07-22 18:02:33 +0800

    :sparkles: Added public files

---
+++ public/precache-manifest.ce68dd8ef5cc7fccbdd7062e00b3d01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b07373f2dfe436adff55fa0f6228a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd2322bdf26b2234a5d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9bd2322bdf26b2234a5d",
+    "url": "/static/js/2.e40a22a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e40a22a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2784017f15ff6f09e51b",
+    "url": "/static/js/main.6127b7e6.chunk.js"
+  },
+  {
+    "revision": "409a7b48f47724504bc4",
+    "url": "/static/js/runtime-main.0053ac68.js"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(express.json());
 app.use(cors());
